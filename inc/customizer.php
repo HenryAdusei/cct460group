ac639fd333af886a090df61263d8884a18a99913
nabeelx22 <nabeelshaikh@live.com>
2016-03-16 00:04:48 -0400

    Uploaded Theme

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Authentic Theme Customizer.
+ *
+ * @package Authentic
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function authentic_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'authentic_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function authentic_customize_preview_js() {
+	wp_enqueue_script( 'authentic_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'authentic_customize_preview_js' );

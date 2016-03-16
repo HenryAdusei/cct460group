e7c62711916db265e33f345e75ad91b27a369557
Nabeel17 <nabeel.jamal@mail.utoronto.ca>
2016-03-16 19:47:44 -0400

    Adding comments for the Flexslider

--- js/flexslider.js
+++ js/flexslider.js
@@ -1,6 +1,7 @@
 //Custom global scripts
 jQuery(document).ready(function($){
 
+// Got the following code from http://flexslider.woothemes.com/basic-carousel.html
 // Can also be used with $(document).ready()
 $(window).load(function() {
   $('.flexslider').flexslider({

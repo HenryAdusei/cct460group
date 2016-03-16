6517ebcba7aef3d942ac3d59e828a4a36eb55255
Nabeel17 <nabeel.jamal@mail.utoronto.ca>
2016-03-16 16:57:31 -0400

    Adding the Flexslider in the Functions.php

--- functions.php
+++ functions.php
@@ -124,8 +124,10 @@ function authentic_scripts() {
 
 	wp_enqueue_script( 'authentic-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'authentic-flexslider', get_template_directory_uri() . '/js/flexslider.js', array(), true);
+	wp_enqueue_script( 'authentic-flexslider', get_stylesheet_directory_uri() . '/js/jquery.flexslider-min.js', array('jquery'), '2.6.0', true );
 	
+	wp_enqueue_script( 'authentic-flexslider.js', get_template_directory_uri() . '/js/flexslider.js', array('jquery'), '1.0', true);
+				
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}

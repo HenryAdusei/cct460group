81624ed56150336b6c90e19feaf12804596f3173
Nabeel17 <nabeel.jamal@mail.utoronto.ca>
2016-03-16 19:17:13 -0400

    Adding the Flexslider.js file

--- js/flexslider.js
+++ js/flexslider.js
@@ -1,6 +1,11 @@
+//Custom global scripts
+jQuery(document).ready(function($){
+
 // Can also be used with $(document).ready()
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide"
   });
 });
+
+}(jQuery));
